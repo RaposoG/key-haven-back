893a195332593bb111f91891d8c14051948fba52
giovani souza <giovani.souza@insidecode.io>
2025-03-21 00:15:18 -0300

    feat(repository): introduce repository module for user collection management
    - Created a new file `module.go` in the `internal/repository` directory to define an FX module for managing the user collection in MongoDB.
    - Implemented a function to provide the `mongo.Collection` for the "users" collection, facilitating easy access to user data.
    - Integrated the `NewUserRepository` function into the module for dependency injection.
    
    These changes establish a dedicated repository module that enhances the application's architecture by promoting separation of concerns and modularity. By utilizing the FX framework for dependency injection, the project becomes more maintainable and scalable, allowing for efficient management of user-related data operations in the future.

---
+++ internal/repository/module.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"key-haven-back/internal/infra/database"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Module(
+	"repository",
+	fx.Provide(
+		func(client database.MongoDBClient) *mongo.Collection {
+			return client.Database("key-haven").Collection("users")
+		},
+		NewUserRepository,
+	),
+)
