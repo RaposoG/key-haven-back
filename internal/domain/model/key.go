e85a4cd2940ce70ac4e290f3903a86400198ada8
Matheus Oliveira 🚀 <teteuoliveira12@gmail.com>
2025-03-25 04:13:42 -0300

    feat(model): adicionar campos de data ao modelo Key
    - Adicionado o campo CreatedAt do tipo time.Time ao modelo Key.
    - Adicionado o campo UpdatedAt do tipo time.Time ao modelo Key.
    
    Essas alterações melhoram o modelo Key, permitindo o rastreamento de quando uma chave foi criada e atualizada, o que é essencial para a gestão e auditoria de dados no sistema.

--- internal/domain/model/key.go
+++ internal/domain/model/key.go
@@ -1,1 +1,13 @@
 package model
+
+import "time"
+
+type Key struct {
+	ID        string `json:"id" bson:"_id"`
+	MasterKey string `json:"master_key" bson:"master_key"`
+
+	UserID string `json:"user_id" bson:"user_id"`
+
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+}
